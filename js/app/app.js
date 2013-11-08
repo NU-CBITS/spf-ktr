78a9156516ea40d88fa79a8933abd95ba0108bd6
Tag0Mag0 <jem.hilton@gmail.com>
2013-11-07 19:18:21 -0600

    updated autoplay, moved the audio to the left, and updated the narrator

--- js/app/app.js
+++ js/app/app.js
@@ -398,11 +398,15 @@ app.actions.loadVideo = function (video) {
 app.actions.loadAudio = function (audiofile) {
 
 	var audioTemplate = function (mp4_location) {
+		// return '<audio style=\"width:100%;\" controls autoplay><source src="' + app.audio_url + mp4_location + '" type="audio/mp3">Your browser does not support the video tag.</video>'
 		// Add in narrarator
-		return '<audio style=\"width:100%;\" controls autoplay><source src="' + app.audio_url + app.config.voice_over + '/' + mp4_location + '" type="audio/mp3">Your browser does not support the video tag.</video>'
+		return '<audio style=\"width:100%;padding-top: 10px;\" controls autoplay><source src="' + app.audio_url + app.config.voice_over + '/' + mp4_location + '" type="audio/mp3">Your browser does not support the video tag.</video>'
 	}
-
-	$(".topRight").html(audioTemplate(audiofile.replace("http://", "").replace("/", "")));
+	if ($('.mainContent a.audio audio').length == 0) {
+		$('.mainContent a.audio').append(audioTemplate(audiofile.replace("http://", "").replace("/", "")));
+	};
+	$('.mainContent a.audio audio')[0].play(); // bc in android html5 autoplay doesn't work
+	// $(".topRight").html(audioTemplate(audiofile.replace("http://", "").replace("/", "")));
 };
 
 app.actions.loadAudioImage = function (filename) {
