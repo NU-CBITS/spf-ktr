1a5dd0493cb3c0f67f6dd18f054dbe35038ab702
Tag0Mag0 <jem.hilton@gmail.com>
2013-11-11 15:41:15 -0600

    moved audio bar to the right of the Listen link

--- js/app/app.js
+++ js/app/app.js
@@ -402,7 +402,7 @@ app.actions.loadAudio = function (audiofile) {
 	var audioTemplate = function (mp4_location) {
 		// return '<audio style=\"width:100%;\" controls autoplay><source src="' + app.audio_url + mp4_location + '" type="audio/mp3">Your browser does not support the video tag.</video>'
 		// Add in narrarator
-		return '<audio style=\"width:100%;padding-top: 10px;\" controls autoplay><source src="' + app.audio_url + "Final Audio/" + app.config.voice_over + '/' + mp4_location + '" type="audio/mp3">Your browser does not support the video tag.</video>'
+		return '<audio controls autoplay><source src="' + app.audio_url + "Final Audio/" + app.config.voice_over + '/' + mp4_location + '" type="audio/mp3">Your browser does not support the video tag.</video>'
 	};
 	if ($('.mainContent #audio-container').length == 0) {
 		$('.mainContent a.audio').after('<div id="audio-container">'+audioTemplate(audiofile.replace("http://", "").replace("/", ""))+'</div>');
